5ef6729b0bd694bcc15b248ffd34cd8d904ae8ca
Jalen Lum <808jalen@gmail.com>
2024-04-18 02:04:10 -1000

    commented out static files

--- api/settings.py
+++ api/settings.py
@@ -128,12 +128,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL='static/'
-STATIC_ROOT = BASE_DIR / 'static'
-STATICFILES_DIR = [
-	BASE_DIR / 'static',
-	BASE_DIR / 'static/icon',
-]
+# STATIC_URL='static/'
+# STATIC_ROOT = BASE_DIR / 'static'
+# STATICFILES_DIR = [
+# 	BASE_DIR / 'static',
+# 	BASE_DIR / 'static/icon',
+# ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field

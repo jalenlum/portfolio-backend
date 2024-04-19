59c345993671bddaeb8a0428afddb1a24bfd28bc
Jalen Lum <808jalen@gmail.com>
2024-04-18 20:55:54 -1000

    added cors to allow all origins

--- api/settings.py
+++ api/settings.py
@@ -46,12 +46,14 @@ INSTALLED_APPS = [
     'main_app',
     'db_file_storage',
     'rest_framework',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -60,6 +62,7 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'api.urls'
+CORS_ALLOW_ALL_ORIGINS = True
 
 TEMPLATES = [
     {
